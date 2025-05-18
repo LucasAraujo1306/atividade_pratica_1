5e079e7336aaf3bf41892b74b1aee49b640afd0f
LucasAraujo1306 <lucasisaac007@gmail.com>
2025-05-17 22:04:37 -0300

    Encapsulamento e Sobrescrita da classe abstract Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,4 +1,7 @@
-public class Pessoa {
+import java.util.Objects;
+
+//abstract: Impedir a instanciação direta: Uma classe abstrata não pode ser instanciada diretamente.
+public abstract class Pessoa {
     private String nome;
     private int idade;
     private String cpf;
@@ -37,6 +40,32 @@ public class Pessoa {
         System.out.println("Nome: "+nome);
         System.out.println("Idade: "+idade);
         System.out.println("CPF"+cpf);
+    }
+
+    //Override: Permite que a classe filha sobrescreva o método da classe pai, garantindo que o comportamento seja específico para cada tipo de pessoa.
+    //ToString: Fornece uma representação em string da classe, útil para depuração e exibição de informações.
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+
+    //Equals: Permite comparar duas instâncias de Pessoa para verificar se são iguais com base em seus atributos.   
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return idade == pessoa.idade && Objects.equals(nome, pessoa.nome) && Objects.equals(cpf, pessoa.cpf);
+    }
+
+    //HashCode: Gera um código hash para a instância da classe, útil para armazenar objetos em coleções como HashMap ou HashSet.
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, idade, cpf);
     }
 
 }
